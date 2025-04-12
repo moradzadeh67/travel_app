45a86a8cfaa5fadde20a5dea8b634c339bd28bab
reza <moradzade.mohamadreza@gmail.com>
2025-04-12 13:41:18 -0700

    second

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expandable_text/expandable_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -32,190 +33,208 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              color: Colors.white,
-              width: double.infinity,
-              height: size.height / 1.8,
-              child: Stack(
-                children: [
-                  Container(
-                    width: double.infinity,
-                    height: size.height / 2.1,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(60),
-                        bottomRight: Radius.circular(60),
-                      ),
-                      image: DecorationImage(
-                        image: AssetImage(travelList[_selectedIndex].image),
-                        fit: BoxFit.cover,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                color: Colors.white,
+                width: double.infinity,
+                height: size.height / 1.8,
+                child: Stack(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      height: size.height / 2.1,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(60),
+                          bottomRight: Radius.circular(60),
+                        ),
+                        image: DecorationImage(
+                          image: AssetImage(travelList[_selectedIndex].image),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                  ),
-                  Positioned(
-                    right: 0,
-                    left: 0,
-                    top: 0,
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(16, 20, 16, 0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    Positioned(
+                      right: 0,
+                      left: 0,
+                      top: 0,
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(16, 20, 16, 0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              width: 50,
+                              height: 50,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color.fromARGB(101, 255, 255, 255),
+                              ),
+                              child: const Icon(CupertinoIcons.arrow_left),
+                            ),
+                            Container(
+                              width: 50,
+                              height: 50,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color.fromARGB(101, 255, 255, 255),
+                              ),
+                              child: const Icon(CupertinoIcons.heart),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      right: 10,
+                      top: 63,
+                      child: SizedBox(
+                        width: 90,
+                        height: double.maxFinite,
+                        child: ListView.builder(
+                          itemCount: travelList.length,
+                          itemBuilder: (context, index) {
+                            return imageItem(index);
+                          },
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 85,
+                      left: size.width / 9,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Container(
-                            width: 50,
-                            height: 50,
-                            decoration: const BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Color.fromARGB(101, 255, 255, 255),
+                          Text(
+                            travelList[_selectedIndex].name,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
                             ),
-                            child: const Icon(CupertinoIcons.arrow_left),
                           ),
-                          Container(
-                            width: 50,
-                            height: 50,
-                            decoration: const BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Color.fromARGB(101, 255, 255, 255),
-                            ),
-                            child: const Icon(CupertinoIcons.heart),
+                          SizedBox(height: 8),
+                          Row(
+                            children: [
+                              Icon(
+                                CupertinoIcons.placemark_fill,
+                                color: Colors.white,
+                              ),
+                              Text(
+                                travelList[_selectedIndex].location,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.normal,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
                     ),
-                  ),
-                  Positioned(
-                    right: 10,
-                    top: 63,
-                    child: SizedBox(
-                      width: 90,
-                      height: double.maxFinite,
-                      child: ListView.builder(
-                        itemCount: travelList.length,
-                        itemBuilder: (context, index) {
-                          return imageItem(index);
-                        },
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 85,
-                    left: size.width / 9,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          travelList[_selectedIndex].name,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        Row(
-                          children: [
-                            Icon(
-                              CupertinoIcons.placemark_fill,
-                              color: Colors.white,
-                            ),
-                            Text(
-                              travelList[_selectedIndex].location,
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.normal,
-                                fontSize: 16,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Container(
-                color: Colors.white,
-                width: double.infinity,
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                            side: BorderSide(
-                              color: Color.fromARGB(60, 0, 0, 0),
-                              width: 1,
-                            ),
-                          ),
-                          child: Container(
-                            width: 85,
-                            height: 85,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                Text('Distance'),
-                                Text(
-                                  travelList[_selectedIndex].distance + 'Km',
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                            side: BorderSide(
-                              color: Color.fromARGB(60, 0, 0, 0),
-                              width: 1,
-                            ),
-                          ),
-                          child: Container(
-                            width: 85,
-                            height: 85,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                Text('Temp'),
-                                Text(travelList[_selectedIndex].temp + 'C'),
-                              ],
-                            ),
-                          ),
-                        ),
-                        Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                            side: BorderSide(
-                              color: Color.fromARGB(60, 0, 0, 0),
-                              width: 1,
-                            ),
-                          ),
-                          child: Container(
-                            width: 85,
-                            height: 85,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                Text('Rating'),
-                                Text(travelList[_selectedIndex].rating),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
                   ],
                 ),
               ),
-            ),
-          ],
+              Container(
+                child: Container(
+                  color: Colors.white,
+                  width: double.infinity,
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              side: BorderSide(
+                                color: Color.fromARGB(60, 0, 0, 0),
+                                width: 1,
+                              ),
+                            ),
+                            child: Container(
+                              width: 85,
+                              height: 85,
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text('Distance'),
+                                  Text(
+                                    travelList[_selectedIndex].distance + 'Km',
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              side: BorderSide(
+                                color: Color.fromARGB(60, 0, 0, 0),
+                                width: 1,
+                              ),
+                            ),
+                            child: Container(
+                              width: 85,
+                              height: 85,
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text('Temp'),
+                                  Text(travelList[_selectedIndex].temp + 'C'),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              side: BorderSide(
+                                color: Color.fromARGB(60, 0, 0, 0),
+                                width: 1,
+                              ),
+                            ),
+                            child: Container(
+                              width: 85,
+                              height: 85,
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text('Rating'),
+                                  Text(travelList[_selectedIndex].rating),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(24, 16, 24, 16),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Description'),
+                            ExpandableText(
+                              'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+                              expandText: 'show more',
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
